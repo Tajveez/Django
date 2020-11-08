165913f79933fe3a1c055cd724d1331149c3cd0d
Tajveez <Tajveez786@gmail.com>
2020-11-08 20:45:01 +0500

    Changes new

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Question, Choice
 
+# https://youtu.be/e1IyzVyrLSU?t=3105
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
