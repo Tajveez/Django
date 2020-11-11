9ef4e37d6cba7d6d978038eda44c491e7645a788
Tajveez <Tajveez786@gmail.com>
2020-11-11 23:40:15 +0500

    changes

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,16 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# Show specific questions and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/result.html', {'question': question})
+
+#get question and display result
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
